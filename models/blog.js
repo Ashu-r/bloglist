830ae7e438823947b018d50a73d84e65de681e73
Ashu-r <randiveashutosh@gmail.com>
2021-05-26 22:29:35 +0530

    init

---
+++ models/blog.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+	title: { type: String, required: true },
+	author: { type: String, required: true },
+	url: { type: String, required: true },
+	likes: Number,
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+	},
+	comments: { type: Array, default: [], required: true },
+});
+
+blogSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
