e020ce3291edf7e322c16cfd8de2176104ed7630
Ashu-r <randiveashutosh@gmail.com>
2021-05-27 13:15:13 +0530

    0.3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('express-async-errors');
 
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
@@ -27,7 +28,8 @@ mongoose
 		logger.error('error connecting to MongoDB', error.message);
 	});
 
-app.use(express.static('build'));
+app.use('/static', express.static(path.join(__dirname + '/public')));
+
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
